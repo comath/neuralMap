b9176d94fa2a9943e87f0a06280619590081b6cc
Sven <scattell@gmail.com>
2017-05-19 18:16:42 -0400

    new mapper.c

--- source/cutils/linAlgSubroutines.h
+++
@@ -1,8 +0,0 @@
-#ifndef _linAlgSubroutines_h
-#define _linAlgSubroutines_h
-#include "structDefs.h"
-ipCacheData * solve(ipMemory *mb);
-
-void printFloatArr(float * arr, uint length);
-void printMatrix(float * arr, uint inDim, uint outDim);
-#endif
